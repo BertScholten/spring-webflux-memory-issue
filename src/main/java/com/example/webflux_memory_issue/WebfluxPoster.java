ac39dc6027dcb5611d6a6beea70283812d1d8157
Bert Scholten <bscholten@dat.nl>
2023-02-17 18:28:43 +0100

    Removed some unnecessary onStatus statements
    These are not important to demonstrate the issue.

--- src/main/java/com/example/webflux_memory_issue/WebfluxPoster.java
+++ src/main/java/com/example/webflux_memory_issue/WebfluxPoster.java
@@ -2,21 +2,14 @@ package com.example.webflux_memory_issue;
 
 import java.time.Duration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
-
-import reactor.core.publisher.Mono;
 
 @Service
 public class WebfluxPoster {
 
-  private static final Logger LOG = LoggerFactory.getLogger(WebfluxPoster.class);
   private static final Duration WEBCLIENT_TIMEOUT = Duration.ofMinutes(1);
 
   private final WebClient fileServerWebClient;
@@ -39,11 +32,6 @@ public class WebfluxPoster {
         .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(object)
         .retrieve()
-        .onStatus(HttpStatus::is5xxServerError, clientResponse -> clientResponse.bodyToMono(String.class).map(message -> {
-          LOG.error("Error while posting with message: {}", message);
-          return new RuntimeException();
-        }))
-        .onStatus(HttpStatus::is4xxClientError, cr -> Mono.error(new ResponseStatusException(cr.statusCode(), cr.statusCode().getReasonPhrase())))
         .toBodilessEntity()
         .block(WEBCLIENT_TIMEOUT);
   }
